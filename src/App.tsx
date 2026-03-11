a04fabfbd328c71a05b8c32ade6abd871bb455b1
laurenpatriciaa <laurenpatricia166@gmail.com>
2026-03-11 15:20:37 +0700

    login mentor

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import SubmitReport from './pages/SubmitReport';
+import RoleSelection from './pages/RoleSelection';
+import MentorLogin from './pages/MentorLogin';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="submit-report" element={<SubmitReport />} />
+        {/* Landing Page: Role Selection */}
+        <Route path="/" element={<RoleSelection />} />
+        
+        {/* Mentor Login */}
+        <Route path="/mentor-login" element={<MentorLogin />} />
+
+        {/* Main Application Layout for Dashboard & Reports */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/submit-report" element={<SubmitReport />} />
         </Route>
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
